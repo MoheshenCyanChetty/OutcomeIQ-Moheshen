3c599c495c5cd9a110034ad1b50f2603f29d9af7
Rodney Takunda Madondo <85015225+RodneyTakundaMadondo@users.noreply.github.com>
2023-11-21 14:09:56 +0200

    modified signin page
    made changes to html and css of sign in page. Added jpeg image for sign in background image.

--- signIn.php
+++ signIn.php
@@ -5,31 +5,45 @@
 //     exit();
 // }
 
-require_once('partials/headSection.php'); //Includes top part of hmtl tags, database connection(which includes constants.php) ,common styles.css, main.js, font-awesome CDN connection(let's u use icons straight from the web without having to download them), .
+// require_once('partials/headSection.php'); 
+//Includes top part of hmtl tags, database connection(which includes constants.php) ,common styles.css, main.js, font-awesome CDN connection(let's u use icons straight from the web without having to download them), .
 
 ?>
 
 <link rel="stylesheet" href="css/sign-in.css"> <!--page custom css file-->
 
-    <title>Sign in to OutcomeIQ</title>
+<title>Sign in to OutcomeIQ</title>
+<style>
+    .signInPageBody{
+    background-image: url('css/homeBackground.jpeg');
+    background-size:cover;
+    background-repeat:no-repeat;
+    display: grid;
+    place-items: center;
+  }
+</style>
 </head>
 <body class="signInPageBody">
+
     <div class="logInformContainer">
+
         <div class="leftSide">
             <div class="logInformHeader">
-                <img class="signInLogo" src="images/LogoBlackTextOutcomeIQ.svg" alt="">
+                <img class="signInLogo" src="images/LogoBlackTextOutcomeIQ.svg" alt="OutcomeIQ Logo">
                 <h4 class="smallGreyText">Sign in to continue</h4>
             </div>
+            
             <form action="index.php" method="POST">
                 <div class="formHolder">
                     <div class="formItem">
-                        <label for="email">Enter Email</label>
-                        <input type="email" id="email">
+                        <label for="email">Email</label>
+                        <input type="email" id="email" name="email">
                     </div>
 
                     <div class="formItem">
-                        <label for="password">Enter Password</label>
-                        <input type="password" id="password">    
+                        <label for="password">Password</label>
+                        <input type="password" id="password" name="password">  
+                        <div class="show-password" onclick="togglePassword()">Show</div> 
                     </div>
 
                     <div class="ForgotPassword">
@@ -38,7 +52,9 @@ require_once('partials/headSection.php'); //Includes top part of hmtl tags, data
                         </div>
                     </div>
 
-                    <button type="submit">Sign In</button>
+                    <div class="btnContainer">
+                        <button type="submit">Sign In</button>
+                    </div>
                 </div>
             </form>
         </div>
@@ -49,4 +65,18 @@ require_once('partials/headSection.php'); //Includes top part of hmtl tags, data
         </div>
     </div>
 </body>
+<script>
+function togglePassword() {// code for the show button in password input
+    var passwordInput = document.getElementById("password");
+    var showPasswordButton = document.querySelector(".show-password");
+
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+        showPasswordButton.textContent = "Hide";
+    } else {
+        passwordInput.type = "password";
+        showPasswordButton.textContent = "Show";
+    }
+}
+</script>
 </html>
