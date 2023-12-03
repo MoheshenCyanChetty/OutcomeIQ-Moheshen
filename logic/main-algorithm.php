14690ea2aa55c713dfea9c1e5025459a4a440e75
Moheshen Chetty <110111560+MoheshenCyanChetty@users.noreply.github.com>
2023-12-03 20:02:00 +0200

    Update main-algorithm.php
    return values in assoc array

--- logic/main-algorithm.php
+++ logic/main-algorithm.php
@@ -22,14 +22,24 @@
                 : MarkCalculator::calc_probability($caTest1Score, $caTest2Score, $requiredFinalMark);
     
             
+            // formatting to 2 decimal places
             $requiredFinalExamMark = number_format(round($requiredFinalExamMark, 2), 2);
             $probability = number_format(round($probability, 2), 2);
             
             $riskLevel = MarkCalculator::calc_RiskLevel($probability);
             $accuracy = $shouldUseAssignmentScore; // boolean for accuracy, if true High Accuracy if false, low accuracy
 
-            $accuracyString = $shouldUseAssignmentScore ? "High Accuracy Prediction": "Low Accuracy Prediciton";
-            
+            $accuracyString = $shouldUseAssignmentScore ? "High Accuracy Prediction": "Low Accuracy Prediciton"; // mostly used for displaying
+           
+            // returns associative array of necessary values
+            return [
+                "requiredFinalMark" => $requiredFinalExamMark,
+                "probability" => $probability,
+                "riskLevel" => $riskLevel,
+                "accuracy" => $accuracy,
+                "accuracyString" => $accuracyString
+            ];
+
         }
 
     }
